78443b23f8896a4bd95e470fe1064c40cde49e73
lucan.chen <chenlucan@hotmail.com>
2015-10-09 17:53:28 +0800

    pod install

---
+++ Podfile
@@ -0,0 +1,5 @@
+
+pod 'WebRTC', :path => "/Users/lucan/projects/jyw/webrtc-ios"
+pod "PubNub", "~> 4.0"
+pod "QBImagePickerController"
+
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,56 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CocoaLumberjack
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack
+#define COCOAPODS_VERSION_MAJOR_CocoaLumberjack 2
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack 0
+#define COCOAPODS_VERSION_PATCH_CocoaLumberjack 0
+
+// CocoaLumberjack/Core
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack_Core
+#define COCOAPODS_VERSION_MAJOR_CocoaLumberjack_Core 2
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack_Core 0
+#define COCOAPODS_VERSION_PATCH_CocoaLumberjack_Core 0
+
+// CocoaLumberjack/Default
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack_Default
+#define COCOAPODS_VERSION_MAJOR_CocoaLumberjack_Default 2
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack_Default 0
+#define COCOAPODS_VERSION_PATCH_CocoaLumberjack_Default 0
+
+// CocoaLumberjack/Extensions
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack_Extensions
+#define COCOAPODS_VERSION_MAJOR_CocoaLumberjack_Extensions 2
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack_Extensions 0
+#define COCOAPODS_VERSION_PATCH_CocoaLumberjack_Extensions 0
+
+// PubNub
+#define COCOAPODS_POD_AVAILABLE_PubNub
+#define COCOAPODS_VERSION_MAJOR_PubNub 4
+#define COCOAPODS_VERSION_MINOR_PubNub 0
+#define COCOAPODS_VERSION_PATCH_PubNub 7
+
+// PubNub/Core
+#define COCOAPODS_POD_AVAILABLE_PubNub_Core
+#define COCOAPODS_VERSION_MAJOR_PubNub_Core 4
+#define COCOAPODS_VERSION_MINOR_PubNub_Core 0
+#define COCOAPODS_VERSION_PATCH_PubNub_Core 7
+
+// QBImagePickerController
+#define COCOAPODS_POD_AVAILABLE_QBImagePickerController
+#define COCOAPODS_VERSION_MAJOR_QBImagePickerController 3
+#define COCOAPODS_VERSION_MINOR_QBImagePickerController 2
+#define COCOAPODS_VERSION_PATCH_QBImagePickerController 0
+
+// WebRTC
+#define COCOAPODS_POD_AVAILABLE_WebRTC
+#define COCOAPODS_VERSION_MAJOR_WebRTC 0
+#define COCOAPODS_VERSION_MINOR_WebRTC 0
+#define COCOAPODS_VERSION_PATCH_WebRTC 1
+
